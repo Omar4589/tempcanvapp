d197d5ddf6f17cb3bb95f1a7a35a96e9bd5b7744
Omar4589 <omar4589@hotmail.com>
2025-10-21 04:02:18 -0500

    fix: use fs read for survey-config.json for Heroku compatibility

--- controllers/survey.js
+++ controllers/survey.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import cfg from "../survey-config.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const cfgPath = path.join(__dirname, "../survey-config.json");
+const cfg = JSON.parse(fs.readFileSync(cfgPath, "utf-8"));
 
 export const survey = Router();
 survey.get("/survey-config", (_req, res) => res.json(cfg));
